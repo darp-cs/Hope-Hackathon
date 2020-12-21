f68dea988048ca800ebec133af26309a86ac7aaf
darp-cs <diegoarod14@gmail.com>
2020-12-21 07:04:35 -0500

    added routing to to pages using local host and also modified index so it uses a home.js file instead of index.js

--- index.js
+++ index.js
@@ -1,24 +1,4 @@
 
-"use strict"
-
-let index = 0;
-const show = () => {
-    let i = 0;
-    const slides = document.getElementsByClassName('slide');
-    for(i=0; i<slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-
-    index = index + 1;
-    if(index > slides.length){
-        index = 1;
-    }
-    slides[index-1].style.display="block";
-    setTimeout(show, 2700);
-
-}
-show();
-
 const request = require("request");
 const express = require('express')
 const path = require('path');
@@ -27,15 +7,25 @@ const app = express()
 const apiKey = 'your api key';
 const router = express.Router();
 
+app.use('/images', express.static(__dirname + '/images'));
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/js', express.static(__dirname + '/js'));
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static('public'));
 
 
-app.get('/', function(req,res){
-    res.sendFile('index.html');
-})
+router.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+    //__dirname : It will resolve to your project folder.
+  });
 
 
+router.get('/pollution',function(req,res){
+res.sendFile(path.join(__dirname+'/html/pollution.html'));
+//__dirname : It will resolve to your project folder.
+});
+
+app.use('/', router);
 const port = process.env.PORT || 3002
 app.listen(port, ()=>{
     console.log(`This server is running on ${port}`)
